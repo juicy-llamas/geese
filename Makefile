bc628e221d813fce7eab3bd6be661cc6713bd91e
Daniel Moylan <demoylan@gmail.com>
2026-01-06 22:58:44 -0500

    IT WORKS

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 
-geese: miniaudio.h
+geese: miniaudio.h main.c
 	# gcc -O3 -Wall main.c -o geese -lSDL2
-	gcc -O3 -Wall $$(pkg-config --cflags --libs gtk4) main.c -o geese
+	gcc -O3 -Wall main.c -o geese $$(pkg-config --cflags --libs gtk4) -lm
 
 miniaudio.h:
 	if [[ ! -f miniaudio.h ]]; then curl https://raw.githubusercontent.com/mackron/miniaudio/master/miniaudio.h -o miniaudio.h; fi
 
 .PHONY: clean
 clean:
-	rm -fr geese 
+	rm -fr geese
 
 .PHONY: install
 install: geese
--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 // Include gtk
+#define MINIAUDIO_IMPLEMENTATION
+#define GNU_SOURCE
+
 #include <gtk/gtk.h>
+#include "gio/gio.h"
 #include "miniaudio.h"
 #include <stdlib.h>
 #include <printf.h>
@@ -9,48 +13,38 @@ struct config {
 };
 
 static void on_activate ( GtkApplication *app, void* user_data ) {
-    struct config* conf = (struct config*)user_data;
-    GError* err;
+  struct config* conf = (struct config*)user_data;
+  GError* err;
 	// Create a new window
 	GtkWidget* window = gtk_application_window_new( app );
-    GdkPixbuf* imgbuf = gdk_pixbuf_new_from_file( conf->image_name, err );
-    if ( err ) {
-        imgbuf = gdk_pixbuf_new_from_file( "geese.jpg", err );
-        if ( err ) {
-            printf( "oh no" );
-            return;
-        }
-    }
-    GtkWidget* image = gtk_image_new_from_pixbuf( imgbuf );
-    gtk_container_add( GTK_WINDOW( window ), image );
-	gtk_window_present( GTK_WINDOW( window ) );
+  GtkWindow* realwind = GTK_WINDOW( window );
+  GtkWidget* image = gtk_picture_new_for_filename( conf->image_name );
+  gtk_window_set_child( realwind, image );
+  gtk_window_set_focus( realwind, image );
+  gtk_widget_activate( image );
+  gtk_window_present( realwind );
 }
 
 int main ( int argc, char** argv ) {
-    // We don't have to free this--I just don't want it to
-    // disappear in case main returns.
-    struct config* conf = malloc( sizeof( struct config ) );
-    conf->image_name = "geese.jpg";
+  // We don't have to free this--I just don't want it to
+  // disappear in case main returns.
+  struct config* conf = malloc( sizeof( struct config ) );
+  conf->image_name = "geese.jpg";
 
-    ma_result result;
-    ma_engine engine;
+  ma_result result;
+  ma_engine engine;
 
-    if (argc < 2) {
-        printf("No input file.");
-        return -1;
-    }
+  result = ma_engine_init(NULL, &engine);
+  if ( result != MA_SUCCESS ) {
+    printf( "Failed to initialize audio engine." );
+    return -1;
+  }
 
-    result = ma_engine_init(NULL, &engine);
-    if ( result != MA_SUCCESS ) {
-        printf( "Failed to initialize audio engine." );
-        return -1;
-    }
-
-    ma_engine_play_sound( &engine, "geese.wav", NULL );
+  ma_engine_play_sound( &engine, "geese.wav", NULL );
 
 	// Create a new GTK application
 	GtkApplication* app = gtk_application_new(
-            "com.example.GtkApplication", G_APPLICATION_FLAGS_NONE );
+            "com.example.GtkApplication", G_APPLICATION_DEFAULT_FLAGS );
 	g_signal_connect( app, "activate", G_CALLBACK( on_activate ), conf );
 	return g_application_run( G_APPLICATION( app ), argc, argv );
 }
